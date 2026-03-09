379ae7873bdd7c5b2462dc9b1638dab9cd831865
Thiez703 <huynhchithien151@gmail.com>
2026-03-10 00:39:18 +0700

    Add files via upload

---
+++ PointTrack/src/test/java/com/teco/pointtrack/PointTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teco.pointtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
